f6638c3fa5280883a9e379aa536ff182528285c5
Kevin K Varughese <kevinkv@stud.ntnu.no>
2018-08-25 11:06:23 +0200

    Changed default user.

--- cache/compiled/files/996e3251678144e2c65f5120f71fba92.yaml.php
+++ cache/compiled/files/996e3251678144e2c65f5120f71fba92.yaml.php
@@ -2,7 +2,7 @@
 return [
     '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
     'filename' => 'C:/xampp/htdocs/user/accounts/admin.yaml',
-    'modified' => 1521109701,
+    'modified' => 1535187791,
     'data' => [
         'email' => 'kevinkv@hotmail.com',
         'fullname' => 'Kevin K Varughese',
@@ -17,6 +17,11 @@ return [
                 'login' => true
             ]
         ],
-        'hashed_password' => '$2y$10$3ojF46FhqPYVAUqJ7gkT.OnR0h28aFxx6bhKDqAehWQINCnnVCVn2'
+        'hashed_password' => '$2y$10$3ojF46FhqPYVAUqJ7gkT.OnR0h28aFxx6bhKDqAehWQINCnnVCVn2',
+        'language' => 'en',
+        'login_attempts' => [
+            
+        ],
+        'twofa_secret' => 'ZCNSI3VX57MDVVAF4Q5NP3CI7LIVS4FC'
     ]
 ];
